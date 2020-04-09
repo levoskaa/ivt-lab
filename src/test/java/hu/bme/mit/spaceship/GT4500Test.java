1d5c09857e140d7b73bbecfa40a0ae91935b4269
levoskaa <laurinyecz.levi@gmail.com>
2020-04-09 17:06:28 +0200

    Add mocks to isolate testing

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,39 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore torpedoStore;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    torpedoStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(torpedoStore, torpedoStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(torpedoStore.isEmpty()).thenReturn(false);
+    when(torpedoStore.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    verify(torpedoStore, times(1)).isEmpty();
+    verify(torpedoStore, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(torpedoStore.isEmpty()).thenReturn(false);
+    when(torpedoStore.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+    verify(torpedoStore, times(2)).isEmpty();
+    verify(torpedoStore, times(2)).fire(1);
   }
-
 }
